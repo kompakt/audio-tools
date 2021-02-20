4ecc0acdf8c25b909b1389f40ee89f745ba0fab4
sirprize <hoegl@backdoor.ch>
2021-02-20 18:03:54 +0100

    Fix kid3-cli issue (AIFF tagging not working on the server)

--- lib/Runner/Kid3Runner.php
+++ lib/Runner/Kid3Runner.php
@@ -14,20 +14,30 @@ use Kompakt\AudioTools\Runner\Exception\RuntimeException;
 class Kid3Runner
 {
     protected $bin = null;
+    protected $tempWorkDir = null;
     protected $returnCode = 0;
-    protected $output = array();
+    protected $output = [];
 
-    public function __construct($bin = 'kid3')
+    public function __construct($bin = 'kid3', $tempWorkDir = '/')
     {
         $this->bin = $bin;
+        $this->tempWorkDir = $tempWorkDir;
     }
 
     public function execute($args)
     {
         $this->returnCode = 0;
-        $this->output = array();
+        $this->output = [];
+
+        $cwd = getcwd(); // Save current working directory
         $cmd = sprintf("%s %s", $this->bin, $args);
+
+        chdir($this->tempWorkDir); // Set working dir to guarantee that kid3 runs from a parent directory relative to the audio file - kid3 tagging doesn't seem to work when run from sibling or child directory relative to audio file
+
+
         $lastLine = exec($cmd, $this->output, $this->returnCode);
+
+        chdir($cwd); // restore working dir
 
         /*if ($this->returnCode > 0)
         {
