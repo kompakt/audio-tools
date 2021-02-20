d1fce1fa63e907dab31e045c59c9f31734c56227
sirprize <hoegl@backdoor.ch>
2021-02-20 19:16:38 +0100

    Undo last commit

--- lib/Runner/Kid3Runner.php
+++ lib/Runner/Kid3Runner.php
@@ -14,14 +14,12 @@ use Kompakt\AudioTools\Runner\Exception\RuntimeException;
 class Kid3Runner
 {
     protected $bin = null;
-    protected $tempWorkDir = null;
     protected $returnCode = 0;
     protected $output = [];
 
-    public function __construct($bin = 'kid3', $tempWorkDir = '/')
+    public function __construct($bin = 'kid3')
     {
         $this->bin = $bin;
-        $this->tempWorkDir = $tempWorkDir;
     }
 
     public function execute($args)
@@ -29,15 +27,8 @@ class Kid3Runner
         $this->returnCode = 0;
         $this->output = [];
 
-        $cwd = getcwd(); // Save current working directory
         $cmd = sprintf("%s %s", $this->bin, $args);
-
-        chdir($this->tempWorkDir); // Set working dir to guarantee that kid3 runs from a parent directory relative to the audio file - kid3 tagging doesn't seem to work when run from sibling or child directory relative to audio file
-
-
         $lastLine = exec($cmd, $this->output, $this->returnCode);
-
-        chdir($cwd); // restore working dir
 
         /*if ($this->returnCode > 0)
         {
